58be0fa592059c39b35928fc6a5f1c734e085753
Angélica <angelik797@icloud.com>
2026-03-02 13:13:17 -0500

    update

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ import cors from "cors";
 import productRoutes from "./routes/product.js";
 import categoryRoutes from "./routes/category.js";
 import { errors } from "celebrate";
+import { loadEnvFile } from "node:process";
+loadEnvFile();
 const PORT = process.env.PORT ?? 3000;
+const DBPASSWORD = process.env.DB_PASSWORD;
 
 const app = express();
 app.use(cors());
@@ -15,8 +18,13 @@ app.use("/product", productRoutes);
 app.use("/category", categoryRoutes);
 app.use(errors());
 
-mongoose.connect("mongodb://localhost:27017/catalogdb");
+mongoose.connect(
+  `mongodb+srv://angelica:${DBPASSWORD}@cluster0.wmzxtad.mongodb.net/?appName=Cluster0`,
+);
 
 app.listen(PORT, function () {
   console.log(`Servidor encendido en el puerto ${PORT}`);
+  console.log(
+    `mongodb+srv://angelica:${DBPASSWORD}@cluster0.wmzxtad.mongodb.net/?appName=Cluster0`,
+  );
 });
